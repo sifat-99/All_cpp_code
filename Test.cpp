b72e345ee9283655195deffa79dbcf983def3bf6
sifat-99 <mdabdurrahmansifat@gmail.com>
2024-09-26 14:18:36 +0600

    updated

--- Test.cpp
+++ Test.cpp
@@ -1,40 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class dataLabel
-{
-public:
-    double length;
-    double width;
-    friend class input;
-};
-class input
-{
-public:
-    dataLabel get_in(dataLabel &in)
-    {
-        cout << "Enter Length & Width: ";
-        cin >> in.length >> in.width;
-        return in;
+
+const int MAX_SIZE = 10000;  // Maximum size of the array (can be adjusted)
+
+// Function to insert an element into a sorted array
+void insertInSortedOrder(int arr[], int& size, int num) {
+    int i = size - 1;
+    // Shift elements to the right until we find the correct position
+    while (i >= 0 && arr[i] > num) {
+        arr[i + 1] = arr[i];
+        i--;
     }
-    friend void area(dataLabel in);
-};
-void area(dataLabel in)
-{
-    double area1, area2;
-    area1 = in.length * in.width;
-    area2 = in.length * in.length;
-    cout << "Area of Rectangle = " << area1 << endl;
-    cout << "Area of Square = " << area2 << endl;
-    if (area1 > area2)
-        cout << "Area of Rectangle large";
-    else
-        cout << "Area of Square large";
+    arr[i + 1] = num;  // Insert the new number
+    size++;  // Increment the size
 }
-int main()
-{
-    dataLabel ob1;
-    input ob2;
-    ob2.get_in(ob1);
-    area(ob1);
+
+// Function to find and remove the median from the sorted array
+int findAndRemoveMedian(int arr[], int& size) {
+    int medianIndex = (size - 1) / 2;  // Smaller middle element for even size
+    int median = arr[medianIndex];
+
+    // Shift elements to the left to remove the median
+    for (int i = medianIndex; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    size--;  // Decrement the size
+    return median;
+}
+
+int main() {
+    int arr[MAX_SIZE];  // Array to store the integers
+    int size = 0;  // Current size of the array
+    int n;
+
+    // Reading input
+    while (cin >> n && n != 0) {
+        if (n > 0) {
+            // Insert the number in the sorted order
+            insertInSortedOrder(arr, size, n);
+        } else if (n == -1) {
+            // Output and remove the median
+            if (size > 0) {
+                int median = findAndRemoveMedian(arr, size);
+                cout << median << endl;
+            }
+        }
+    }
+
     return 0;
 }
